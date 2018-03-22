b33c20792149c2f20e3c99c9c722430e04c75f6b
omeiregman <=>
2018-03-22 03:18:33 +0100

    page setup, nav setup

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//NAVIGATION DRAWER
+
+var bodyElement = document.body,
+  content = document.querySelector('.content-wrapper'),
+  openButton = document.getElementById('open-button'),
+  closeButton = document.getElementById('close-button');
+  var isOpen = false;
+
+openButton.addEventListener('click', toggle);
+
+if (closeButton) {
+  closeButton.addEventListener('click', toggle);
+}
+
+content.addEventListener('click', function() {
+  isOpen = bodyElement.classList.contains('js--menu-open');
+  if (isOpen) toggle();
+});
+
+function toggle() {
+    bodyElement.classList.toggle('js--menu-open');
+}
